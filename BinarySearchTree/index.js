a3523e0bd291a3a872da3e02f120ba5aa37f85b0
Abhivera <abhijitverma2016@gmail.com>
2024-08-02 00:42:13 +0530

    complete bst datastructure

--- BinarySearchTree/index.js
+++ BinarySearchTree/index.js
@@ -1,69 +1,111 @@
-class TreeNode{
-  constructor(value){
-      this.value = value;
-      this.left = null;
-      this.right = null;
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
-function  insertAtTree(root,value){
-  if(root==null){
-      return new TreeNode(value)
+function insertAtTree(root, value) {
+  if (root == null) {
+    return new TreeNode(value);
   }
-  if(value>root.value){
-      root.right = Insert(root.right,value)
-  }
-  else if(value<root.value){
-      root.left = Insert(root.left,value)
+  if (value > root.value) {
+    root.right = Insert(root.right, value);
+  } else if (value < root.value) {
+    root.left = Insert(root.left, value);
   }
   return root;
 }
 
-function search(root,target){
-  if(root == null){
-      return false;
+function search(root, target) {
+  if (root == null) {
+    return false;
   }
-  if(target == root.value){
-      return true;
-  }
-  else if(target>root.value){
-      return search(root.right,target)
-  }
-  else if(target<root.value){
-      return search(root.left,target)
+  if (target == root.value) {
+    return true;
+  } else if (target > root.value) {
+    return search(root.right, target);
+  } else if (target < root.value) {
+    return search(root.left, target);
   }
 }
 function minValueNode(root) {
-    let curr = root;
-    while (curr && curr.left) {
-      curr = curr.left;
-    }
-    return curr;
+  let curr = root;
+  while (curr !== null && curr.left !== null) {
+    curr = curr.left;
   }
-function remove(root,val){
-    if (root==null)
+  return curr;
+}
+
+function remove(root, value) {
+  if (root == null) {
     return null;
+  }
+  if (value > root.value) {
+    root.right = remove(root.right, value);
+  } else if (value < root.value) {
+    root.left = remove(root.left, value);
+  } else {
+    let minNode = minValueNode(root.right);
+    root.value = minNode.value;
+    root.right = remove(root.right, minNode.value);
+  }
+  return root;
+}
 
-    if (val>root.val){
-    root.right = remove(root.right,val)
-   }
-    else if (val<root.val){
-    root.left = remove(root.left,val)
-    }
-    else {
-        if(root.left==null){
-            return root.right;
-        }
-        else if(root.right==null){
-            return root.left;
-        }
-        else {
-            let minNode = minValueNode(root.right)
-            root.val = minNode.val
-            root.right=remove(root.right,minNode.value)
+//Depth First Traversal
 
-        }
+function preOrderTraversal(root) {
+  let result = [];
+  if (root !== null) {
+    result.push(root.value);
+    preOrderTraversal(root.left);
+    preOrderTraversal(root.right);
+  }
 
-    }
-    return root
+  return result;
+}
+function inOrderTraversal(root) {
+  let result = [];
+  if (root !== null) {
+    inOrderTraversal(root.left);
+    result.push(root.value);
+    inOrderTraversal(root.right);
+  }
+  return result;
+}
+function postOrderTraversal(root) {
+  let result = [];
+  if (root !== null) {
+    postOrderTraversal(root.left);
+    postOrderTraversal(root.right);
+    result.push(root.value);
+  }
+  return result;
+}
 
+// Breadth First Traversal
+
+function levelOrderTraversal(root){
+  if(root===null){
+      return []
+  }
+  let queue =[]
+  queue.push(root)
+  let result =[]
+  while(queue.length>0){
+      let levelSize = queue.length;
+      let currentLevel =[];
+      for(let i = 0;i<levelSize;i++){
+      let current  = queue.shift()
+      currentLevel.push(current.value)
+      if(current.left!==null){
+          queue.push(current.left)
+      }
+      if(current.right!==null){
+          queue.push(current.right)
+      }}
+      result.push(currentLevel)
+  }
+  return result
 }
